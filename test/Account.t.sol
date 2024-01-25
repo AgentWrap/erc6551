7b45cd7ad87c927a25ddceb97b220c6943fea71a
AlchemistDAO <agentwrap@gmail.com>
2024-01-25 12:26:55 +0800

    ✔️ Requto

--- test/Account.t.sol
+++ test/Account.t.sol
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.21 <0.9.0;
+
 import { PRBTest } from "@prb/test/PRBTest.sol";
-import { AccountRegistry } from "src/AccountRegistry.sol";
-import { Account } from "src/Account.sol";
+import { ERC6551Registry } from "src/ERC6551Registry.sol";
+import { ERC6551Account } from "src/ERC6551Account.sol";
 
 contract Accounr_Test is PRBTest {
-    AccountRegistry public registry;
-    Account public account;
+    ERC6551Registry public registry;
+    ERC6551Account public account;
+
     function setUp() public virtual {
-        registry = new AccountRegistry();
-        account = new Account();
+        registry = new ERC6551Registry();
+        account = new ERC6551Account();
     }
 
-    function test_Account_Eq () public {
-        bytes32 salt = bytes32(type(uint256).max-1);
-        assertEq(registry.createAccount(address(account), salt, block.chainid, address(account), 1),
-                registry.account(address(account), salt, block.chainid, address(account), 1));
+    function test_Account_Eq() public {
+        bytes32 salt = bytes32(type(uint256).max - 1);
+        assertEq(
+            registry.createAccount(address(account), salt, block.chainid, address(account), 1),
+            registry.account(address(account), salt, block.chainid, address(account), 1)
+        );
     }
 }
-
