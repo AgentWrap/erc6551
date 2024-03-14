a45654d61cd675f85635f5427b1547cd6ebaa85c
wangshouh <wongshouhao@outlook.com>
2024-03-14 22:36:08 +0800

    Fix test

--- test/Account.t.sol
+++ test/Account.t.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.21 <0.9.0;
 import { PRBTest } from "@prb/test/PRBTest.sol";
-import { AccountRegistry } from "src/AccountRegistry.sol";
-import { Account } from "src/Account.sol";
+import { ERC6551Registry } from "src/ERC6551Registry.sol";
+import { ERC6551Account } from "src/ERC6551Account.sol";
 
 contract Accounr_Test is PRBTest {
-    AccountRegistry public registry;
-    Account public account;
+    ERC6551Registry public registry;
+    ERC6551Account public account;
     function setUp() public virtual {
-        registry = new AccountRegistry();
-        account = new Account();
+        registry = new ERC6551Registry();
+        account = new ERC6551Account();
     }
 
     function test_Account_Eq () public {
